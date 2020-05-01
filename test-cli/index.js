4feedd9ba635223829098bfc2949caa75bceca98
郭建利 <guojianli@ihappy.net.cn>
2020-05-02 00:06:28 +0800

    生成测试代码

--- test-cli/index.js
+++ test-cli/index.js
@@ -1,10 +1,24 @@
 const path = require('path')
 
 module.exports = class TestCli {
+  getTestSource(methodName, fileName, isClass = false) {
+    return `
+      test('${'TEST ' + methodName}生成', () => {
+        const ${isClass ? '{' + methodName + '}' : methodName} = require(${
+      '../' + fileName
+    })
+        const ret = ${methodName}()
+        // expect(ret).toBe('return test')
+      })
+    `
+  }
+
   getTestFileName(fileName) {
     const dirName = path.dirname(fileName)
     const baseName = path.basename(fileName)
     const extName = path.extname(fileName)
+    console.log(dirName, baseName, extName)
+
     const testName = baseName.replace(extName, `.spec${extName}`)
     return path.format({
       root: dirName + '/__test__/',
